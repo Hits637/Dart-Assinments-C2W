2b1a4dde91a34e667289b83c4240687d55ba56d2
hits637 <hiteshpatil.skn.entc@gmail.com>
2024-08-28 11:35:17 +0530

    A7

---
+++ Assignment 7 Pattern-1/Program10.dart
@@ -0,0 +1,19 @@
+import "dart:io";
+
+void main() {
+  print("Enter number of rows");
+  int rows = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= rows; i++) {
+    int temp = i;
+    for (int j = 1; j <= rows; j++) {
+      
+      stdout.write(temp++);
+      
+      stdout.write(" ");
+      
+    }
+
+    print("");
+  }
+}
